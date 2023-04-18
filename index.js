f273de03d633a3064e5a7db6ceca2ce165e434a6
Aleš Sedláček <xsedla11@fi.muni.cz>
2023-04-18 20:38:23 +0200

    Remove unused api

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ app.get("/", (req, res) => {
   res.send("Express JS on Vercel");
 });
 
-app.get("/ping", (req, res) => {
-  res.send("pong 🏓");
-});
-
 const port = process.env.PORT || 8080;
 
 app.listen(port, (err, res) => {
